4659e2e62a768e26f1fa7bd79ca8f4c1cc67eb76
Luke Hiatt <lhiatt7@ivytech.edu>
2020-03-31 12:17:15 -0400

    Add project files.

---
+++ Module 2 - HOL 1 C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_2___HOL_1_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
